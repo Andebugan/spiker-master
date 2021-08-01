5ab6cab71d18f663afb5bad01a98c0248d7c1b78
Andebugan <andreibugaenko2012@mail.ru>
2021-08-01 03:19:54 +0300

    Added death wall

---
+++ Spiker_new/Assets/Scenes/MainScene/Game/GamePrefabs/Level/Enemies/KillOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnCollision : MonoBehaviour
+{
+    PlayerController playerController;
+    
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerController.Kill();
+        }
+    }
+}
